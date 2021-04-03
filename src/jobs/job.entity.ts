a233c7f042ca9289813fa4b060d6b654007a6457
abdulazizladan <abdulazizladan@gmail.com>
2021-04-03 07:02:17 +0100

    task: Complete job entity

--- src/jobs/job.entity.ts
+++ src/jobs/job.entity.ts
@@ -1,14 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Job{
 
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     posted_by: string;
+
+    @Column()
     title: string;
+
+    @Column()
     description: string;
+
+    @Column()
     country: string;
+
+    @Column()
     state: string;
+
+    @Column()
     lga: string;
+
+    @Column()
     street_address: string;
+
+    @Column()
     is_available: boolean;
+
+    @Column()
     budget: number;
 
     constructor(
