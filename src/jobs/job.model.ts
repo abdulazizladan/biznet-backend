161e198b4adbad652ec9521cd399536ae6da2337
abdulazizladan <abdulazizladan@gmail.com>
2021-04-03 07:09:04 +0100

    task: Complete jiob model

--- src/jobs/job.model.ts
+++ src/jobs/job.model.ts
@@ -12,6 +12,29 @@ export class Job{
     budget : number;
     date_added : number;
 
-    constructor(){
+    constructor(
+        id : number,
+        added_by : string,
+        title : string,
+        description : string,
+        country : string,
+        state : string,
+        lga : string,
+        street_address : string,
+        is_available : boolean,
+        budget : number,
+        date_added : number,
+    ){
+        this.id = id;
+        this.added_by = added_by;
+        this.title = title;
+        this.description = description;
+        this.country = country;
+        this.state = state;
+        this.lga = lga;
+        this.street_address = street_address;
+        this.is_available = is_available;
+        this.budget = budget;
+        this.date_added = date_added;
     }
 }
