7ccf63b3dc1a65ab6821a1ef161d5e4bb3325da5
abdulazizladan <abdulazizladan@gmail.com>
2021-04-04 06:25:01 +0100

    task: Add Products service and entity

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(
+        @InjectRepository(Product)
+        private productRepository: Repository<Product>
+    ){}
+
+    findAll() : any{
+        return this.productRepository.find()
+    }
+
+    find( id: number ){
+        return this.productRepository.findOne(id);
+    }
+
+    addProduct( productData: string ){
+        return productData;
+    }
+
+    removeProduct( id: number ){
+        return 'removing product ' + id;
+    }
+
+}
