0340aed2cd46cca7150811d9ca277bafc86fb65c
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-04-17 23:17:21 +0100

    task: create Jobs controller

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { JobsService } from './jobs.service';
 
+@ApiTags('Jobs')
 @Controller('jobs')
 export class JobsController {
+
+    constructor( private JobsService: JobsService){
+
+    }
 
     @Get()
     getJobs(){
