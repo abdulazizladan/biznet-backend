3b9af89e4dccaaf8f376e3c7762d9b3994fbbd5c
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-04-20 06:53:18 +0100

    task: Init Articles controller

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Observable } from 'rxjs';
 import { Article } from './article.model';
 import { ArticleService } from './article.service';
 
+@ApiTags('Article')
 @Controller('article')
 export class ArticleController{
 
     constructor(private articleService: ArticleService){
 
     }
+
+    @Get()
+    findAll(): string{
+        return "All articles";
+    }
+
+    @Get('find')
+    find(): string{
+        return "finding one"
+    }
+
 
     //@Post()
     //create(@Body()articleEntry: Article): Observable<Article>{
