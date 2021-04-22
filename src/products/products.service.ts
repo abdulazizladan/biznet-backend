a8eeb634aa33b5f80977508d677724f595e33acd
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-04-22 06:03:40 +0100

    task: generate product TypeORM model from product.entity

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,16 +6,12 @@ import { Product } from './product.entity';
 @Injectable()
 export class ProductsService {
     constructor(
-        @InjectRepository(Product)
-        private productRepository: Repository<Product>
     ){}
 
     findAll() : any{
-        return this.productRepository.find()
     }
 
     find( id: number ){
-        return this.productRepository.findOne(id);
     }
 
     addProduct( productData: string ){
