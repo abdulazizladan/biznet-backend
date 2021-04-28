c5a7abb818486895da0e369a340505c81c8874a6
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-04-28 06:04:03 +0100

    task: comment port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  //port 3000
   await app.listen(3000);
 }
 bootstrap();
