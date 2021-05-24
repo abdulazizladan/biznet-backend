068348fdff9d2e58cacfd06fbfa6fe77cea6335b
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-05-24 06:16:11 +0100

    task: Init products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+    @Get()
+    findAll() {
+
+    }
+
+    @Get()
+    findOne() {
+
+    }
+
+    @Post()
+    create() {
+
+    }
+
+    @Put()
+    edit() {
+
+    }
+
+    @Delete()
+    remove() {
+        
+    }
+}
